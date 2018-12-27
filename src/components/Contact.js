3254d2e6c4fa81beac8f6db869e838b50496641e
Russ Wells <russ@russdigital.com>
2018-12-27 15:20:15 -0800

    dhs v2 inital commit

---
+++ src/components/Contact.js
@@ -0,0 +1,55 @@
+import React from 'react'
+
+const Contact = (props) => (
+    <section id="contact">
+        <div className="inner">
+            <section>
+                <form method="post" action="#">
+                    <div className="field half first">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" name="name" id="name" />
+                    </div>
+                    <div className="field half">
+                        <label htmlFor="email">Email</label>
+                        <input type="text" name="email" id="email" />
+                    </div>
+                    <div className="field">
+                        <label htmlFor="message">Message</label>
+                        <textarea name="message" id="message" rows="6"></textarea>
+                    </div>
+                    <ul className="actions">
+                        <li><input type="submit" value="Send Message" className="special" /></li>
+                        <li><input type="reset" value="Clear" /></li>
+                    </ul>
+                </form>
+            </section>
+            <section className="split">
+                <section>
+                    <div className="contact-method">
+                        <span className="icon alt fa-envelope"></span>
+                        <h3>Email</h3>
+                        <a href="#">digitalhyperspace369@gmail.com</a>
+                    </div>
+                </section>
+                <section>
+                    <div className="contact-method">
+                        <span className="icon alt fa-phone"></span>
+                        <h3>Phone</h3>
+                        <span>(760) 936-1704</span>
+                    </div>
+                </section>
+                <section>
+                    <div className="contact-method">
+                        <span className="icon alt fa-home"></span>
+                        <h3>Address</h3>
+                        <span>4930 Santa Monica Ave. #7<br />
+                        San Diego, CA 92107<br />
+                        United States of America</span>
+                    </div>
+                </section>
+            </section>
+        </div>
+    </section>
+)
+
+export default Contact
---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Footer = (props) => (
+    <footer id="footer">
+        <div className="inner">
+            <ul className="icons">
+                <li><a href="https://twitter.com/digitalhyper_" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
+                <li><a href="https://www.facebook.com/digitalhyperspace" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
+                <li><a href="https://www.instagram.com/digitalhyperspace" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
+                <li><a href="https://github.com/russjwells" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
+                <li><a href="https://www.linkedin.com/in/russjwells" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
+            </ul>
+            <ul className="copyright">
+                <li>&copy; Digital Hyperspace 2018 – 2019</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li>
+            </ul>
+        </div>
+    </footer>
+)
+
+export default Footer
