cd3e262c03f221fb9cc5dd32e8ac48190bfbc50b
Russ Wells <russ@russdigital.com>
2018-12-28 14:56:15 -0800

    menu cleanup

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -9,7 +9,6 @@ const Menu = (props) => (
                 <li><Link onClick={props.onToggleMenu} to="/">Home</Link></li>
             </ul>
             <ul className="actions vertical">
-                <li><a href="#contact" className="button special fit">Send A Message</a></li>
                 <li><a href="#" className="button fit">Client Log In</a></li>
             </ul>
         </div>
