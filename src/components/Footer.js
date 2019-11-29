360b796e125a72294916d2be2e2cdb007a8d85c1
Russ Wells <russ@russdigital.com>
2019-11-29 15:43:10 -0800

    insta link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,10 @@ const Footer = (props) => (
             <ul className="icons">
                 <li><a href="https://twitter.com/digitalhyper_" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
                 <li><a href="https://www.facebook.com/digitalhyperspace" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
-                <li><a href="https://www.instagram.com/digitalhyperspace" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
+                <li><a href="https://www.instagram.com/hyperspace.im" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
             </ul>
             <ul className="copyright">
-                <li>&copy; Digital Hyperspace 2018 – 2019</li>
+                <li>&copy; Hyperspace 2019</li>
             </ul>
         </div>
     </footer>
